cefa468fade84132ffcf9b8e2de811d3a8edb3a6
quan930 <1583090876@qq.com>
2022-01-30 09:56:48 +0800

    hook v0.0.2

--- hook/README.md
+++ hook/README.md
@@ -2,5 +2,5 @@
 image repo: [lilqcn/hook:v0.0.1](https://hub.docker.com/repository/docker/lilqcn/hook)
 run
 ```shell
-docker 
+docker run -p 8080:8080 -d lilqcn/hook:v0.0.2
 ```
--- hook/internal/controller/GithubCon.go
+++ hook/internal/controller/GithubCon.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"github.com/emicklei/go-restful"
 	"hook/internal/pojo"
+	"k8s.io/klog/v2"
 )
 
 type GithubCon struct {
@@ -18,5 +19,6 @@ func (c GithubCon) Test(request *restful.Request, response *restful.Response)  {
 	body := make([]byte, len)  // 创建存放请求实体的字节切片
 	request.Request.Body.Read(body)        // 调用 Read 方法读取请求实体并将返回内容存放到上面创建的字节切片
 	str := string(body[:])
+	klog.Info(str)
 	response.WriteEntity(pojo.NewResponse(200, "successful", str).Body)
 }
