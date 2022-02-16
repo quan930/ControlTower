8c7026a44317729822c44d39aae5fe3de35a2b8f
quan930 <1583090876@qq.com>
2022-02-16 13:57:45 +0800

    builder init

---
+++ builder/internal/git/git.go
@@ -0,0 +1,44 @@
+package git
+
+import (
+	"fmt"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/config"
+	"github.com/go-git/go-git/v5/plumbing"
+	"k8s.io/klog/v2"
+	"os"
+)
+
+func Clone(gitUrl string, workPath string) *git.Repository {
+	klog.Info("git clone ", gitUrl)
+
+	repository, err := git.PlainClone(workPath, false, &git.CloneOptions{
+		URL:      gitUrl,
+		Progress: os.Stdout,
+	})
+	if err != nil {
+		klog.Info(err)
+		return nil
+	}
+	return repository
+}
+
+func Checkout(repo *git.Repository, branchName string) error {
+	klog.Info("git fetch")
+	err := repo.Fetch(&git.FetchOptions{
+		RefSpecs: []config.RefSpec{"refs/*:refs/*", "HEAD:refs/heads/HEAD"},
+	})
+	if err != nil {
+		klog.Info(err)
+	}
+	klog.Info("git checkout ", branchName)
+	wt, err := repo.Worktree()
+	if err != nil {
+		klog.Info(err)
+	}
+	return wt.Checkout(&git.CheckoutOptions{
+		Branch: plumbing.ReferenceName(fmt.Sprintf("refs/heads/%s", branchName)),
+		Force:  true,
+	})
+	return nil
+}
